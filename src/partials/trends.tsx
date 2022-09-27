4be32f6c8ec95ff4f225bb808e5390a00c985ef2
Pashmeena Noor <pashmeena.noor@metaschool.so>
2022-09-27 18:58:01 +0500

    links added and user image

--- src/partials/trends.tsx
+++ src/partials/trends.tsx
@@ -6,25 +6,21 @@ function Trends() {
     <>
       <div className="trendings">
         <h3>Trends for you</h3>
-        <a href="#!" title="">
-          <strong>#Web3nft</strong>
+        <a href="http://web3auth.io/" title="" target="_blank">
+          <strong>#Web3Auth</strong>
           <span>125k tweets</span>
         </a>
-        <a href="#!" title="">
-          <strong>#Beauty</strong>
+        <a href="http://goerlifaucet.com/" title="" target="_blank">
+          <strong>#GoerliFaucet</strong>
           <span>98k tweets</span>
         </a>
-        <a href="#!" title="">
-          <strong>#FloodsInPakistan2022</strong>
+        <a href="http://twitter.com/hashtag/EthereumMerge" title="" target="_blank">
+          <strong>#EthereumMerge</strong>
           <span>53k tweets</span>
         </a>
-        <a href="#!" title="">
-          <strong>#IndvsPak</strong>
+        <a href="https://metaschool.so/" title="" target="_blank">
+          <strong>#Metaschool</strong>
           <span>42k tweets</span>
-        </a>
-        <a href="#!" title="">
-          <strong>#AsiaCup2022</strong>
-          <span>22k tweets</span>
         </a>
       </div>
     </>
